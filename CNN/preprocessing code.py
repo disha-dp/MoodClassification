2696d8b59a8ee7accc22b3a916920d4f5ce26e05
akhilabattula <akhilaanand23@gmail.com>
2016-04-30 05:23:20 -0700

    Added files via upload

--- CNN/preprocessing code.py
+++ CNN/preprocessing code.py
@@ -1,10 +1,5 @@
 
-# coding: utf-8
-
-# In[1]:
-
 import cPickle
-import numpy
 import json
 import ast
 import codecs
@@ -12,114 +7,182 @@ import pickle
 import numpy
 import numpy as np 
 from collections import defaultdict
+
+vocabulary = defaultdict(float)
+imp_data = []
+lyricvector=[]
+
+def read_write(stopwords,words):
+   
+    fp=open('songdump2.txt','r')
+    temp=fp.read()
+    word_map=ast.literal_eval(temp)
+    features={}
+    foldnumber=10
+    for k,label in word_map.items():
+        print label
+        #print k - at each song level
+        lyricwords= k.decode('utf-8').split(' ')
+        #strip
+        lyricwords = [w.strip('\n, )(.-24') for w in lyricwords]
+        #remove stop words
+        lyricwords=[j for j in lyricwords if not j in stopwords]
+        for w in lyricwords: #at word level
+            vocabulary[w]+=1
+            if w in words:
+                index=words.index(w)
+                lyricvector.append(embeddings[index],)
+        features[label]=lyricvector     # ADD song vectors and song labels 
+        if 'sad' in label:
+            datastruct  = {"y":0, 
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'tareef' in label:
+            datastruct  = {"y":1,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'romantic' in label:
+            datastruct  = {"y":2,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'devotional' in label:
+            datastruct  = {"y":3,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'happy' in label:
+            datastruct  = {"y":4,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'funny' in label:
+            datastruct  = {"y":5,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'party' in label:
+            datastruct  = {"y":6,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'satire' in label:
+            datastruct  = {"y":7,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'naughty' in label:
+            datastruct  = {"y":8,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'playful' in label:
+            datastruct  = {"y":9,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'masti' in label:
+            datastruct  = {"y":10,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'patriotic' in label:
+            datastruct  = {"y":11,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'item' in label:
+            datastruct  = {"y":12,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        
+        if 'rock' in label:
+            datastruct  = {"y":13,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+            
+        if 'pop' in label:
+            datastruct  = {"y":14,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'friendship' in label:
+            datastruct  = {"y":15,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'motivational' in label:
+            datastruct  = {"y":16,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'remix' in label:
+            datastruct  = {"y":17,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'love' in label:
+            datastruct  = {"y":18,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'dance' in label:
+            datastruct  = {"y":19,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        if 'remix' in label:
+            datastruct  = {"y":20,
+                      "text":k,                             
+                      "num_words": len(k.decode('utf-8').split(' ')),
+                      "split": np.random.randint(0,foldnumber)}
+        imp_data.append(datastruct)
+
 words, embeddings = pickle.load(open('polyglot-hi.pkl', 'rb'))
-
-
-# In[2]:
-
-word= words[10]
-print word
-print type(embeddings[10])
-
-
-# In[3]:
-
-vocab = defaultdict(float)
-revs = []
-
-
-# In[6]:
-
-stopwordfile = codecs.open("stop.txt", encoding='utf-8')
-stopwords = [x.strip() for x in stopwordfile.readlines()]
-
-#print str(stopwords)
-
-s=open('songdump2.txt','r')
-c=s.read()
-diction=ast.literal_eval(c)
-features={}
-cv=5
-for k,label in diction.items():
-    print label
-    #print k - at each song level
-    song_vec=[]
-    song_words= k.decode('utf-8').split(' ')
-    #strip
-    song_words = [w.strip('\n, )(.-24') for w in song_words]
-    #remove stop words
-    song_words=[x for x in song_words if not x in stopwords]
-    
-    for w in song_words: #at word level
-        vocab[w]+=1
-        if w in words:
-            idx=words.index(w)
-            song_vec.append(embeddings[idx],)
-        #else:
-            #print w
-            #print 'not in the dictionary:' + w + ' end'
-        #print song_vec
-    features[label]=song_vec     # ADD song vectors and song labels 
-    if 'sad' in label:
-        datum  = {"y":1, 
-                      "text":k,                             
-                      "num_words": len(k.decode('utf-8').split(' ')),
-                      "split": np.random.randint(0,cv)}
-    else:
-        datum  = {"y":0,
-                      "text":k,                             
-                      "num_words": len(k.decode('utf-8').split(' ')),
-                      "split": np.random.randint(0,cv)}
-        
-    revs.append(datum)
-    
-#print (features)
-#print vocab
-#print revs
-
-w2v = {}
-for w in vocab:
-    if w in words:
-        idx=words.index(w)
-        w2v[w]=embeddings[idx]
-        
-def add_unknown_words(word_vecs, vocab, min_df=1, k=64):
+#word= words[10]
+#print word
+#print type(embeddings[10])
+def vectors_drive(lyricvector, k=64):
     """
-    For words that occur in at least min_df documents, create a separate word vector.    
-    0.25 is chosen so the unknown vectors have (approximately) same variance as pre-trained ones
+    Get word matrix. W[index] is the vector for word indexed by index
     """
-    for word in vocab:
-        if word not in word_vecs and vocab[word] >= min_df:
-            word_vecs[word] = np.random.uniform(-0.25,0.25,k)   
-
-def get_W(word_vecs, k=64):
-    """
-    Get word matrix. W[i] is the vector for word indexed by i
-    """
-    vocab_size = len(word_vecs)
-    word_idx_map = dict()
+    vocab_size = len(lyricvector)
+    wordindexmap = dict()
     W = np.zeros(shape=(vocab_size+1, k), dtype='float32')            
     W[0] = np.zeros(k, dtype='float32')
     i = 1
-    for word in word_vecs:
-        W[i] = word_vecs[word]
-        word_idx_map[word] = i
+    for word in lyricvector:
+        W[i] = lyricvector[word]
+        wordindexmap[word] = i
         i += 1
-    return W, word_idx_map
+    return W, wordindexmap
 
-add_unknown_words(w2v,vocab)
-W, word_idx_map = get_W(w2v)
-rand_vecs = {}
-add_unknown_words(rand_vecs, vocab)
-W2, _ = get_W(rand_vecs)
-cPickle.dump([revs, W, W2, word_idx_map, vocab], open("mr.p", "wb"))
-print W2
+
+
+stopwordfile = codecs.open("stop.txt", encoding='utf-8')
+stopwords = [x.strip() for x in stopwordfile.readlines()]
+#print str(stopwords)
+read_write(stopwords)
+#print (features)
+#print vocab
+#print imp_data
+word2vec = {}
+for w in vocabulary:
+    if w in words:
+        boundary=words.index(w)
+        word2vec[w]=embeddings[boundary]
+W, wordmap = vectors_drive(word2vec)
+cPickle.dump([imp_data, W, wordmap, vocabulary], open("datafile.p", "wb"))
+
+
+
         
 
 
 # ## 
 
 # In[ ]:
-
-
-
